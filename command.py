22999d7887ec9f6555d31968046ef5af2c628d5e
Alexander Mendez <bluespace@gmail.com>
2011-12-08 00:07:46 -0800

    new stream

--- command.py
+++ command.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """\
-Simple g-code streaming script for grbl
+Simple g-code streaming Terminal
 """
 
 import serial, time, readline
@@ -36,36 +36,4 @@ s.close()
 
 
 # 
-# #!/usr/bin/env python
-# """\
-# Simple g-code streaming script for grbl
-# """
-# 
-# import serial
-# import time
-# 
-# # Open grbl serial port
-# s = serial.Serial('/dev/tty.usbmodem0000',9600)
-# 
-# # Open g-code file
-# f = open('somefile.gcode','r');
-# 
-# # Wake up grbl
-# s.write("\r\n\r\n")
-# time.sleep(2)   # Wait for grbl to initialize
-# s.flushInput()  # Flush startup text in serial input
-# 
-# # Stream g-code to grbl
-# for line in f:
-#     l = line.strip() # Strip all EOL characters for streaming
-#     print 'Sending: ' + l,
-#     s.write(l + '\n') # Send g-code block to grbl
-#     grbl_out = s.readline() # Wait for grbl response with carriage return
-#     print ' : ' + grbl_out.strip()
-# 
-# # Wait here until grbl is finished to close serial port and file.
-# raw_input("  Press <Enter> to exit and disable grbl.")
-# 
-# # Close file and serial port
-# f.close()
-# s.close()
+
